96de3655a9e881f455d0f4fbf4def7c226324b1a
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-10 23:21:41 +0300

    f

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,8 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
 
-import casheImages from './casheImages';
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("code is ready");
-  casheImages();  
+// import casheImages from './casheImages';
+document.addEventListener('DOMContentLoaded', () => {  
+  // casheImages();  
 });
 
 
--- src/js/casheImages.js
+++ src/js/casheImages.js
@@ -1,32 +1,30 @@
-// import img1 from '/static/img/mainButton_hover.png';
+// const casheImages = () => {
+//   var casheImagesArr = [
+//     './static/img/mainButton_hover.png',
+//     './static/img/mainButton_pressed.png',
+//     './static/img/button_connect_hover.png',
+//     './static/img/button_connect_pressed.png',
+//     './static/img/button_egg_hover.png',
+//     './static/img/button_egg_pressed.png',
+//     './static/img/button_skull_hover.png',
+//     './static/img/button_skull_pressed.png',
+//     './static/img/switch_toggle_01.png',
+//     './static/img/rockerSwitch_blue_01.png',
+//     './static/img/rockerSwitch_green_01.png',
+//     './static/img/warningSign_on.png',
+//     './static/img/greenLight_on.png',
+//     './static/img/redLight_on.png',
+//     './static/img/yellowLight_on.png'
+//   ]
+//   const preCacheImages = () =>{
+//     let img=[];
+//     casheImagesArr.map((item,ind)=>{
+//       console.log("item:",item);
+//         img[ind] = new Image();
+//         img[ind].src = item;
+//     });
+//   };
+//   preCacheImages();
+// }
 
-const casheImages = () => {
-  var casheImagesArr = [
-    './static/img/mainButton_hover.png',
-    './static/img/mainButton_pressed.png',
-    './static/img/button_connect_hover.png',
-    './static/img/button_connect_pressed.png',
-    './static/img/button_egg_hover.png',
-    './static/img/button_egg_pressed.png',
-    './static/img/button_skull_hover.png',
-    './static/img/button_skull_pressed.png',
-    './static/img/switch_toggle_01.png',
-    './static/img/rockerSwitch_blue_01.png',
-    './static/img/rockerSwitch_green_01.png',
-    './static/img/warningSign_on.png',
-    './static/img/greenLight_on.png',
-    './static/img/redLight_on.png',
-    './static/img/yellowLight_on.png'
-  ]
-  const preCacheImages = () =>{
-    let img=[];
-    casheImagesArr.map((item,ind)=>{
-      console.log("item:",item);
-        img[ind] = new Image();
-        img[ind].src = item;
-    });
-  };
-  preCacheImages();
-}
-
-export default casheImages;
+// export default casheImages;
