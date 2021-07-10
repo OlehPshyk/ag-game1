8150fb0e4ff445e4079bfac85b2f42be25ff25e8
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-11 02:59:15 +0300

    df

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,12 @@
 
 // import casheImages from './casheImages';
 import interactiveGame from './interactiveGame';
-import './audiosynth.js';
+// import './audiosynth.js';
 document.addEventListener('DOMContentLoaded', () => {  
   // casheImages();  
   interactiveGame();  
+  // console.log("Synth instanceof AudioSynth:",AudioSynth);
+  // console.log("Synth instanceof AudioSynth:",Synth instanceof AudioSynth);
 });
 
 
