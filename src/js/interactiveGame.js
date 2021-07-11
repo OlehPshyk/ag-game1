56666cb53c4373cd836d494a0e0ac727f8a0d92c
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-11 22:47:48 +0300

    f

--- src/js/interactiveGame.js
+++ src/js/interactiveGame.js
@@ -17,9 +17,7 @@ const interactiveGame = () => {
   }
   const resolveGameMedium = (event) => {    
     const g3Btn = event.target.closest('[data-id="g3-btn"]');
-    if(g3Btn){
-      console.log("g3Btn"); 
-      console.log("g3Btn note:",g3Btn.dataset.note);     
+    if(g3Btn){          
       synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);      
     }
   }
