6dd562b51162832e8f2ccc736a60f1b935443479
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-11 22:44:39 +0300

    f

--- src/js/app.js
+++ src/js/app.js
@@ -5,25 +5,13 @@
 import interactiveGame from './interactiveGame';
 
 document.addEventListener('DOMContentLoaded', () => {  
-  var AudioSynth = require('audiosynth');
-
+  
   
   // casheImages();  
   interactiveGame(); 
   
-  // var AudioContext = window.AudioContext || window.webkitAudioContext;
-  // var context = new AudioContext();
-  // var Synth = new AudioSynth(context);
-  // Synth.setOscWave(0); // Sine Wave
-  // Synth.setOscWave(1); // Square Wave
-  // Synth.setOscWave(2); // Sawtooth Wave
-  // Synth.setOscWave(3); // Triangle Wave
-  // // function(MIDINote, amplitude, filterOffset, currentTime)
-  // Synth.playNote(69, 1.0, 1.0, 0);
-  // // Set notes using traditional note names
-  // Synth.playNote(Synth.noteToMIDI('A', 4), 1.0, 1.0, 0);
-  // var piano = Synth.createInstrument('piano');
-  // piano.play('C', 4, 2);
+  
+  
 
 });
 
--- src/js/interactiveGame.js
+++ src/js/interactiveGame.js
@@ -1,11 +1,30 @@
+var AudioSynth = require('audiosynth');
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var synth = new AudioSynth(context);
+// Default oscillator wave is sawtooth
+synth.setOscWave(0); // Sine Wave
+synth.setOscWave(1); // Square Wave
+synth.setOscWave(2); // Sawtooth Wave
+synth.setOscWave(3); // Triangle Wave
+
 const interactiveGame = () => {  
   const resolveGameTop = (event) => {
-    const tg1Btn = event.target.closest('[data-id="tg1-btn"]');
+    const tg1Btn = event.target.closest('[data-id="tg1-btn"]');    
     if(tg1Btn){
       tg1Btn.classList.toggle('active');
+    }    
+  }
+  const resolveGameMedium = (event) => {    
+    const g3Btn = event.target.closest('[data-id="g3-btn"]');
+    if(g3Btn){
+      console.log("g3Btn"); 
+      console.log("g3Btn note:",g3Btn.dataset.note);     
+      synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);      
     }
   }
   document.querySelector('.js-game-top').addEventListener('click', resolveGameTop);
+  document.querySelector('.js-game-medium').addEventListener('click', resolveGameMedium);
 }
 
 export default interactiveGame;
