b0170d98d30506237787952bb68b6fc98e5eeffb
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-11 23:20:02 +0300

    f

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,9 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
-
 // import casheImages from './casheImages';
-
 import interactiveGame from './interactiveGame';
-
-document.addEventListener('DOMContentLoaded', () => {  
-  
-  
-  // casheImages();  
-  interactiveGame(); 
-  
-  
-  
-
+document.addEventListener('DOMContentLoaded', () => { 
+  // casheImages();    
+  interactiveGame();
 });
-
-
 if (module.hot) {
   module.hot.accept();
 }
--- src/js/interactiveGame.js
+++ src/js/interactiveGame.js
@@ -1,13 +1,3 @@
-var AudioSynth = require('audiosynth');
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
-var synth = new AudioSynth(context);
-// Default oscillator wave is sawtooth
-synth.setOscWave(0); // Sine Wave
-synth.setOscWave(1); // Square Wave
-synth.setOscWave(2); // Sawtooth Wave
-synth.setOscWave(3); // Triangle Wave
-
 const interactiveGame = () => {  
   const resolveGameTop = (event) => {
     const tg1Btn = event.target.closest('[data-id="tg1-btn"]');    
@@ -15,13 +5,24 @@ const interactiveGame = () => {
       tg1Btn.classList.toggle('active');
     }    
   }
+  document.querySelector('.js-game-top').addEventListener('click', resolveGameTop);
+
+  var AudioSynth = require('audiosynth');
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext();
+  var synth = new AudioSynth(context);
+  // Default oscillator wave is sawtooth
+  // synth.setOscWave(0); // Sine Wave
+  // synth.setOscWave(1); // Square Wave
+  // synth.setOscWave(2); // Sawtooth Wave
+  synth.setOscWave(3); // Triangle Wave
   const resolveGameMedium = (event) => {    
     const g3Btn = event.target.closest('[data-id="g3-btn"]');
-    if(g3Btn){          
-      synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);      
+    if(g3Btn){ 
+      // (MIDINote, amplitude, filterOffset, currentTime)         
+      synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);         
     }
-  }
-  document.querySelector('.js-game-top').addEventListener('click', resolveGameTop);
+  }  
   document.querySelector('.js-game-medium').addEventListener('click', resolveGameMedium);
 }
 
