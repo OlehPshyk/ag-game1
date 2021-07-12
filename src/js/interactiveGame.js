e1d9ff35732cb634a8c4ae401112f4129ea3d5ef
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-13 00:37:33 +0300

    f

--- src/js/interactiveGame.js
+++ src/js/interactiveGame.js
@@ -27,17 +27,16 @@ const interactiveGame = () => {
   // synth.setOscWave(1); // Square Wave
   // synth.setOscWave(2); // Sawtooth Wave
   synth.setOscWave(3); // Triangle Wave
+  // let startSound;
   const resolveGameMedium = (event) => {    
     const g3Btn = event.target.closest('[data-id="g3-btn"]');
     if(g3Btn){ 
       // function(MIDINote, amplitude, filterOffset, currentTime)
       // synth.playNote(69, 1.0, 1.0, 0);
       // Turn up stereo delay
-      synth.setDelayFeedback(0.1);
+      synth.setDelayFeedback(0.2);
       // Set delay time to tempo
-      synth.setDelayTimeTempo(110, 0.25);
-      // (MIDINote, amplitude, filterOffset, currentTime)         
-      synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);   
+      synth.setDelayTimeTempo(110, 0.25);        
       // Set filter cuttoff
       synth.setFilterCutoff(0.2);
       // Increase amplitude release time
@@ -45,10 +44,17 @@ const interactiveGame = () => {
       // Set filter envelope modulation amount
       synth.setFilterEnvMod(0.8);
       // Set filter attack time
-      synth.setFilterAttackTime(0.9);      
+      synth.setFilterAttackTime(0.9);  
+      // (MIDINote, amplitude, filterOffset, currentTime)         
+      synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0);  
+      // startSound = setInterval(()=>{synth.playNote(synth.noteToMIDI(g3Btn.dataset.note||'C', 4), 1.0, 1.0, 0)}, 100);   
     }
   }  
-  document.querySelector('.js-game-medium').addEventListener('click', resolveGameMedium);
+  // const stopSound = () =>{
+  //   clearInterval(startSound);
+  // }
+  document.querySelector('.js-game-medium').addEventListener('mousedown', resolveGameMedium);
+  // document.querySelector('.js-game-medium').addEventListener('mouseup', stopSound);
 }
 
 export default interactiveGame;
