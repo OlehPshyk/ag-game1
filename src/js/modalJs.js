b2c40e4601d4a1d3994a5c5db988646432646e19
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-16 01:26:03 +0300

    f

--- src/js/modalJs.js
+++ src/js/modalJs.js
@@ -87,7 +87,7 @@ const modalJs = () => {
     let qMint = e.target.querySelector('[data-q]').value;
     console.log("quantity Mint:",qMint);
     console.log("total Mint:",total);
-    document.location.href=`${document.location.origin}/second.html`;
+    document.location.href=`${document.location.origin}${pathname}second.html`;
   }
   
   inputValue = parseInt(qInput.value);
