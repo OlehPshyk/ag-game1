72dccc0f21d79aa5e5aaf40e8612b9cf7fd33651
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-16 02:54:39 +0300

    g

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,9 @@ import modalJs from './modalJs';
 document.addEventListener('DOMContentLoaded', () => { 
   // casheImages();    
   interactiveGame();
-  // if(document.getElementById('popup-1')) {
+  if(document.getElementById('popup-1')) {
     modalJs();
-  // }
+  }
 });
 if (module.hot) {
   module.hot.accept();
