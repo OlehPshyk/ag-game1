679db2a36319ec3d30ad05d00fbdad75d7d652d3
Oleh Pshyk <pshyk.oleg@gmail.com>
2021-07-28 22:15:38 +0300

    rem DOMContentLoaded

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 // import casheImages from './casheImages';
 import interactiveCdb from './interactiveCdb';
 import modalJs from './modalJs';
-document.addEventListener('DOMContentLoaded', () => { 
+// document.addEventListener('DOMContentLoaded', () => { 
   // casheImages();    
   interactiveCdb();
   // if(document.getElementById('popup-1')) {
     modalJs();
   // }
-});
+// });
 if (module.hot) {
   module.hot.accept();
 }
